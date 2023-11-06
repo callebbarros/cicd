3a79e5aa6cc583f0701538f7587a7480ed2c1ded
callebbarros <146237907+callebbarros@users.noreply.github.com>
2023-11-06 11:09:07 -0300

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello, Gitlab CI/CD Deploy")
+    
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello, Gitlab CI/CD Test")
+    
+if __name__ == "__main__":
+    main()
